ed48f506fa2d2029a029f902e1903279403bc742
pukpuk555 <jester2540@gmail.com>
2024-05-15 14:54:35 +0700

    complete

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1
+
+let queue = ["Sofia", "David", "Juan"];
+
+// 2
+
+queue.push("Sara", "Augustin");
+
+queue.shift();
+
+//3
+
+queue.splice(1, 0, "Renata");
+queue.push("Elena");
+
+console.log(queue);
